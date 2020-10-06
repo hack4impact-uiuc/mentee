395170c9580164a710646640ce3822cd13922c8e
Leonardo Galindo <batmanlup2@gmail.com>
2020-10-06 15:40:19 -0500

    🎥 VideoLink model (#11)
    * VideoLinks Model Created
    
    * Added VideoLinks to __init__.py
    
    * Deleted get-pip.py
    
    * VideoLinks Missing import  and __init__
    
    * New Line :(
    
    * Fixed Naming Issues and EmbeddedDoc
    
    * Added gitignore for vscode
    
    * Changed Name To Video
    
    * Changed Tags to Tag and Newline:
    
    * New Line in .gitignore

---
+++ backend/api/models/Video.py
@@ -0,0 +1,19 @@
+from api.core import Mixin
+from .base import db
+from mongoengine import *
+
+
+class Video(EmbeddedDocument, Mixin):
+    """Video Collection"""
+
+    def __init__(self, title, url, tag):
+        self.title = title
+        self.url = url
+        self.tag = tag
+
+    title = StringField(required=True)
+    url = StringField(required=True)
+    tag = StringField(required=True)
+
+    def __repr__(self):
+        return f"<Video title: {self.title}, URL: {self.url}, tag: {self.tag}>"
--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -5,7 +5,8 @@ from .base import db
 from .Email import Email
 from .Person import Person
 from .Education import Education
+from .Video import Video
 
-__all__ = ["db", "Email", "Person", "Education"]
+__all__ = ["db", "Email", "Person", "Education", "Video"]
 
 # You must import all of the new Models you create to this page
