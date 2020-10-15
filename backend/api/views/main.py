ed26e063c17b7e15126a9e69a28d4563334b9ad8
Daniel Moon <36148173+AzureQDragon@users.noreply.github.com>
2020-10-15 18:17:23 -0500

    PUT mentor profile (#18)
    * Added mentor profile model
    
    * add import to init.py
    
    * Fixed formatting and imports
    
    * "Update .gitignore"
    
    * Remove .vscode
    
    * Added update_mentor method for mentorprofile
    
    * fix: check for data to replace
    
    * Create embedded fields
    
    * refactor data checking
    
    * reformat with black
    
    * update comments
    
    * fix: create list of videos + refactor
    
    * format with black

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from api.models import db, Person, Email, MentorProfile
+from api.models import db, Person, Email, Education, Video, MentorProfile
 from api.core import create_response, serialize_list, logger
 
 main = Blueprint("main", __name__)  # initialize blueprint
@@ -67,3 +67,57 @@ def create_person():
     return create_response(
         message=f"Successfully created person {new_person.name} with id: {new_person.id}"
     )
+
+
+# PUT requests for /mentor
+@main.route("/mentor/<id>", methods=["PUT"])
+def edit_mentor(id):
+    data = request.get_json()
+
+    logger.info("Data received: %s", data)
+
+    # Try to retrieve Mentor profile from database
+    try:
+        mentor = MentorProfile.objects.get(id=id)
+    except:
+        msg = "No mentor with that id"
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+
+    # Edit fields or keep original data if no added data
+    mentor.name = data.get("name", mentor.name)
+    mentor.professional_title = data.get(
+        "professional_title", mentor.professional_title
+    )
+    mentor.specializations = data.get("specializations", mentor.specializations)
+    mentor.languages = data.get("languages", mentor.languages)
+    mentor.offers_group_appointments = data.get(
+        "offers_group_appointments", mentor.offers_group_appointments
+    )
+    mentor.offers_in_person = data.get("offers_in_person", mentor.offers_in_person)
+    mentor.biography = data.get("biography", mentor.biography)
+    mentor.linkedin = data.get("linkedin", mentor.linkedin)
+    mentor.website = data.get("website", mentor.website)
+    mentor.picture = data.get("picture", mentor.picture)
+
+    # Create education object
+    if "education" in data:
+        education_data = data.get("education")
+        mentor.education = Education(
+            education_level=education_data.get("education_level"),
+            majors=education_data.get("majors"),
+            school=education_data.get("school"),
+            graduation_year=education_data.get("graduation_year"),
+        )
+
+    # Create video objects for each item in list
+    if "videos" in data:
+        video_data = data.get("videos")
+        mentor.videos = [
+            Video(title=video.get("title"), url=video.get("url"), tag=video.get("tag"))
+            for video in video_data
+        ]
+
+    mentor.save()
+
+    return create_response(status=200, message=f"Success")
