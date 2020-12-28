39db47758e1df7d2f7666b8d0d4e9da2f4967839
Lam Tran <Lam7150@gmail.com>
2020-12-27 21:34:03 -0500

    Implemented Mentor Filtering Feature (#143)
    Co-authored-by: Angela Luo <ael49021@gmail.com>

--- frontend/src/components/pages/Gallery.js
+++ frontend/src/components/pages/Gallery.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { fetchMentors } from "../../utils/api";
 import MentorCard from "../MentorCard";
+import { Input, Checkbox } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { LANGUAGES, SPECIALIZATIONS } from "../../utils/consts";
 
 import "../css/Gallery.scss";
 
 function Gallery() {
   const [mentors, setMentors] = useState([]);
+  const [specializations, setSpecializations] = useState([]);
+  const [languages, setLanguages] = useState([]);
+  const [query, setQuery] = useState();
 
   useEffect(() => {
     async function getMentors() {
@@ -28,28 +34,74 @@ function Gallery() {
     return output;
   }
 
+  function getFilteredMentors() {
+    return mentors.filter((mentor) => {
+      // matches<Property> is true if no options selected, or if mentor has AT LEAST one of the selected options
+      const matchesSpecializations =
+        specializations.length == 0 ||
+        specializations.some((s) => mentor.specializations.indexOf(s) >= 0);
+      const matchesLanguages =
+        languages.length == 0 ||
+        languages.some((l) => mentor.languages.indexOf(l) >= 0);
+      const matchesName =
+        !query || mentor.name.toUpperCase().includes(query.toUpperCase());
+
+      return matchesSpecializations && matchesLanguages && matchesName;
+    });
+  }
+
   return (
-    <div className="gallery-mentor-container">
-      {mentors.map((mentor, key) => (
-        <MentorCard
-          key={key}
-          name={mentor.name}
-          languages={mentor.languages}
-          professional_title={mentor.professional_title}
-          location={mentor.location}
-          specializations={mentor.specializations}
-          website={mentor.website}
-          linkedin={mentor.linkedin}
-          id={mentor._id["$oid"]}
-          lesson_types={getLessonTypes(
-            mentor.offers_group_appointments,
-            mentor.offers_in_person
-          )}
-          image={mentor.image}
+    <div className="gallery-container">
+      <div className="gallery-filter-container">
+        <div className="gallery-filter-header">Filter By:</div>
+        <Input
+          placeholder="Search by name"
+          prefix={<SearchOutlined />}
+          style={styles.searchInput}
+          onChange={(e) => setQuery(e.target.value)}
         />
-      ))}
+        <div className="gallery-filter-section-title">Specializations</div>
+        <Checkbox.Group
+          defaultValue={specializations}
+          options={SPECIALIZATIONS}
+          onChange={(checked) => setSpecializations(checked)}
+        />
+        <div className="gallery-filter-section-title">Languages</div>
+        <Checkbox.Group
+          defaultValue={languages}
+          options={LANGUAGES}
+          onChange={(checked) => setLanguages(checked)}
+        />
+      </div>
+      <div className="gallery-mentor-container">
+        {getFilteredMentors().map((mentor, key) => (
+          <MentorCard
+            key={key}
+            name={mentor.name}
+            languages={mentor.languages}
+            professional_title={mentor.professional_title}
+            location={mentor.location}
+            specializations={mentor.specializations}
+            website={mentor.website}
+            linkedin={mentor.linkedin}
+            id={mentor._id["$oid"]}
+            lesson_types={getLessonTypes(
+              mentor.offers_group_appointments,
+              mentor.offers_in_person
+            )}
+            image={mentor.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+const styles = {
+  searchInput: {
+    borderRadius: 10,
+    marginBottom: 5,
+  },
+};
 
 export default Gallery;
