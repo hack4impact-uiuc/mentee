42ce70ffffb1d8f94ac52ec14a723845d0ce9440
Lam Tran <Lam7150@gmail.com>
2021-01-26 12:51:48 -0500

    Made Mentor Profile Mobile Friendly (#171)
    Made Mentor Profile Mobile Friendly

--- backend/api/utils/constants.py
+++ backend/api/utils/constants.py
@@ -1,4 +1,10 @@
-AUTH_URL = "https://h4i-mentee-auth-omega.vercel.app/"
+import os
+
+AUTH_URL = (
+    "h4i-mentee-auth.vercel.app"
+    if os.environ.get("DEPLOYMENT") == "prod"
+    else "https://mentee-auth-dev.vercel.app"
+)
 
 # Template ID that can be created through sendgrid's UI
 # https://sendgrid.com/docs/ui/sending-email/how-to-send-an-email-with-dynamic-transactional-templates/
--- frontend/src/components/MenteeNavHeader.js
+++ frontend/src/components/MenteeNavHeader.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import { Layout } from "antd";
 import { withRouter } from "react-router-dom";
 import { isLoggedIn } from "utils/auth.service";
 import MenteeButton from "./MenteeButton";
+import MenteeVerificationModal from "./MenteeVerificationModal";
 
 import "./css/Navigation.scss";
 
 import MenteeLogo from "../resources/mentee.png";
-import MenteeVerificationModal from "./MenteeVerificationModal";
+import MenteeLogoSmall from "../resources/menteeSmall.png";
 
 const { Header } = Layout;
 
 function MenteeNavHeader({ history }) {
+  const isMobile = useMediaQuery({ query: `(max-width: 500px)` });
+
   return (
     <Header className="navigation-header">
       <div className="navigation-mentee-flexbox">
         <div>
           <NavLink to="/">
-            <img src={MenteeLogo} alt="Mentee" className="mentee-logo" />
+            <img
+              src={isMobile ? MenteeLogoSmall : MenteeLogo}
+              alt="Mentee"
+              className="mentee-logo"
+            />
           </NavLink>
         </div>
         <div>
@@ -26,6 +34,7 @@ function MenteeNavHeader({ history }) {
             <MenteeVerificationModal
               content={<b>Find a Mentor</b>}
               theme="light"
+              width="45%"
               onVerified={() => {
                 history.push({
                   pathname: "/gallery",
@@ -37,7 +46,7 @@ function MenteeNavHeader({ history }) {
           <NavLink to="/login">
             <span className="navigation-header-button">
               <MenteeButton
-                width="125px"
+                width="45%"
                 content={
                   <b>{isLoggedIn() ? "Your Profile" : "Mentor Log In"}</b>
                 }
