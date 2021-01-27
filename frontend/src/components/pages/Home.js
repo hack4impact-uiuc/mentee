26c011ea1d2ac31275691995c76a9c81644406ce
Angela Luo <ael49021@gmail.com>
2021-01-27 09:47:22 -0600

    home page wording

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -10,11 +10,9 @@ function Home({ history }) {
     <div className="home-background">
       <div className="home-content">
         <div className="home-text-container">
-          <h1 className="home-header">Welcome to Mentee</h1>
+          <h1 className="home-header">Welcome to MENTEE</h1>
           <p className="home-text">
-            Find a mentors from a diverse pool of backgrounds with experience in
-            23+ specializations, 15 different languages at locations all across
-            the world.
+          Find a global mentor now...
           </p>
           <br />
           <MenteeVerificationModal
