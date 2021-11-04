c421aaa4c8d2c0b756ed244b0bb567c0f3f2e87f
Zayyan Faizal <zfaizal2@illinois.edu>
2021-11-04 17:11:09 -0500

    🤦🏼‍♀️ Create Direct Message Backend Model (#402)
    * add direct message modal
    
    * formatting

---
+++ backend/api/models/DirectMessage.py
@@ -0,0 +1,15 @@
+from api.core import Mixin
+from .base import db
+from flask_mongoengine import Document
+from mongoengine import *
+
+
+class DirectMessage(Document, Mixin):
+    body = StringField(required=True)
+    message_read = BooleanField(required=True)
+    sender_id = ObjectIdField(required=True)
+    recipient_id = ObjectIdField(required=True)
+    created_at = DateTimeField(required=True)
+
+    def __repr__(self):
+        return f"<DirectMessage:{self.body} \n Sent by:{self.sender_id} to :{self.recipient_id}>"
