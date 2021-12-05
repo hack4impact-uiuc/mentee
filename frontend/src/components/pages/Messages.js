572068b4b220415b4c89611bd104cc8543948847
leundai <batmanlup2@gmail.com>
2021-12-04 23:11:01 -0600

    😡 some guy broke dev

--- frontend/src/components/pages/Messages.js
+++ frontend/src/components/pages/Messages.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import "../css/Messages.scss";
 import useAuth from "../../utils/hooks/useAuth";
-import { MENTEE_GALLERY_PAGE, MENTOR_GALLERY_PAGE } from "../../utils/consts";
+import { BASE_URL } from "../../utils/consts";
 import MessagesSidebar from "components/MessagesSidebar";
 import { Layout } from "antd";
 import MessagesChatArea from "components/MessagesChatArea";
@@ -14,8 +14,6 @@ function Messages(props) {
   const [latestConvos, setLatestConvos] = useState([]);
   const [activeMessageId, setActiveMessageId] = useState("");
   const [messages, setMessages] = useState([]);
-
-  const URL = "http://localhost:5000";
 
   const { profileId } = useAuth();
 
