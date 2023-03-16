cd8c1b95cfc4aec7ebb447cc7e485a7441551e06
SolMon9099 <101564382+SolMon9099@users.noreply.github.com>
2023-03-16 15:49:50 -0400

    👷🏿‍♂️ add header of new train page (#658)
    * email notification for new training
    
    * remove admin@only  for get train data
    
    * add header of new training check page

--- frontend/src/components/pages/NewTrainingConfirm.js
+++ frontend/src/components/pages/NewTrainingConfirm.js
@@ -20,49 +20,57 @@ function NewTrainingConfirm({ accountType, id }) {
     getNewTraining();
   }, [id]);
   return (
-    <div className="trainpart">
-      <div className="train_list">
-        {loading ? <h1>Loading ...</h1> : ""}
-        <List>
-          {train._id && (
-            <List.Item key={id}>
-              <h1 className="chapter">{train.name}</h1>
-              <p className="trainingDesc">{train.description}</p>
-              {train.typee === TRAINING_TYPE.VIDEO && (
-                <ReactPlayer width={800} height={600} o url={train.url} />
-              )}
-              {train.typee === TRAINING_TYPE.LINK && (
-                <a
-                  className="external-link"
-                  href={train.url}
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  {train.url}
-                </a>
-              )}
-              {train.typee === TRAINING_TYPE.DOCUMENT && (
-                <Button
-                  onClick={async () => {
-                    let response = await getTrainVideo(train.id);
-                    downloadBlob(response, train.file_name);
-                  }}
-                >
-                  {train.file_name}
-                </Button>
-              )}
-            </List.Item>
-          )}
-        </List>
-      </div>
-      <div className="btnContainer">
-        <div
-          className={`applySubmit2`}
-          onClick={async () => {
-            window.location.href = "/";
-          }}
-        >
-          confirm
+    <div className="">
+      <h1 className="new-train-header">MENTEE Required New Trainings</h1>
+      <p className="new-train-text">
+        Thank you for your time to further develop your skills and contribute to
+        our community!
+        <br /> Please complete these new training before the end of next month.
+      </p>
+      <div className="trainpart">
+        <div className="train_list">
+          {loading ? <h1>Loading ...</h1> : ""}
+          <List>
+            {train._id && (
+              <List.Item key={id}>
+                <h1 className="chapter">{train.name}</h1>
+                <p className="trainingDesc">{train.description}</p>
+                {train.typee === TRAINING_TYPE.VIDEO && (
+                  <ReactPlayer width={800} height={600} o url={train.url} />
+                )}
+                {train.typee === TRAINING_TYPE.LINK && (
+                  <a
+                    className="external-link"
+                    href={train.url}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    {train.url}
+                  </a>
+                )}
+                {train.typee === TRAINING_TYPE.DOCUMENT && (
+                  <Button
+                    onClick={async () => {
+                      let response = await getTrainVideo(train.id);
+                      downloadBlob(response, train.file_name);
+                    }}
+                  >
+                    {train.file_name}
+                  </Button>
+                )}
+              </List.Item>
+            )}
+          </List>
+        </div>
+        <div className="btnContainer">
+          <div
+            className={`applySubmit2`}
+            onClick={async () => {
+              window.location.href = "/";
+            }}
+          >
+            confirm
+          </div>
         </div>
       </div>
     </div>
