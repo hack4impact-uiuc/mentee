65771baa95bc4687c218520c2a2f49742fb01350
leundai <leogalindofrias@gmail.com>
2023-05-29 12:53:58 -0600

    😁 Update public locales file names

--- frontend/src/utils/i18n.js
+++ frontend/src/utils/i18n.js
@@ -17,7 +17,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: "en-us",
+    fallbackLng: "en-US",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
