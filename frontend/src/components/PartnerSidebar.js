e12eedc0d78567b8ce8ccdd4457e5cfdc043dfbe
SolMon9099 <101564382+SolMon9099@users.noreply.github.com>
2023-06-25 13:23:43 -0400

    🖌️ Mobile design (#736)

--- frontend/src/components/PartnerSidebar.js
+++ frontend/src/components/PartnerSidebar.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { UserOutlined, CalendarOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  CalendarOutlined,
+  MailOutlined,
+} from "@ant-design/icons";
 import Sidebar from "./Sidebar";
 import { useTranslation } from "react-i18next";
+import { useAuth } from "utils/hooks/useAuth";
 
 function PartnerSidebar(props) {
   const { t } = useTranslation();
-
+  const { role } = useAuth();
   const pages = {
     profile: {
       name: t("sidebars.profile"),
       path: "/profile",
       icon: <UserOutlined />,
+    },
+    message: {
+      name: t("common.messages"),
+      path: "/messages/" + role,
+      icon: <MailOutlined />,
     },
   };
 
