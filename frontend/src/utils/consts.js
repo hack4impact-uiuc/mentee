3f281b680b13191f34a1d0337d2e7429b9070699
leundai <leogalindofrias@gmail.com>
2023-07-07 11:19:28 -0600

    🌎 Update consts env variables

--- frontend/src/utils/consts.js
+++ frontend/src/utils/consts.js
@@ -1,5 +1,5 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
-export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
+export const IS_DEVELOPMENT = process.env.REACT_APP_ENV === "development";
 
 export const BASE_URL = IS_PRODUCTION
   ? IS_DEVELOPMENT
