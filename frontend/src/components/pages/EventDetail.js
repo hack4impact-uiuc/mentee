a170e64d6f02cc8953ee3d916601e528e9c0d5e2
robert-dm <35821871+robert-dm@users.noreply.github.com>
2024-06-20 12:23:11 -0300

    Revert "🧑🏽‍⚕️ Freddy branch (#1076)"
    This reverts commit 1b075375c1b054e0ca631deb1de1f3b1bbc94489.

--- frontend/src/components/pages/EventDetail.js
+++ frontend/src/components/pages/EventDetail.js
@@ -70,7 +70,7 @@ function EventDetail({ match }) {
 
   return (
     <>
-      {role !== ACCOUNT_TYPE.ADMIN && event && event.role && !event.role.includes(role) ? (
+      {event && !event.role.includes(role) ? (
         <Result
           status="403"
           title="403"
